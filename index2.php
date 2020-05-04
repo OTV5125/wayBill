5b25366f19a282666c3d1641a351c103331b8e1f
crysismzf <stas-petrushenko@mail.ru>
2020-05-04 14:08:26 +0300

    Добавлены доб инпуты

--- index2.php
+++ index2.php
@@ -109,72 +109,73 @@
 
 </div>
 
-<div class="wrapper-items">
-    <div class="wrapper-item">
-        <div class="petrol-list">
-            Старый пробег <span></span> км.<br>
-            Новый прбег <span></span> км.<br>
-            <br>
-            Остаток <span class="balance"><?=$balance['balance']?></span> л.<br>
-            Дата последней заправки <span class="petrol"><?=$balance['last_date']?></span><br>
-            Всего бензина <span class="sumBalance"><?=$balance['balance']?></span> л.<br>
-            Осталось километров <span class="sumKm"><?=round($balance['balance']/(11/100))?></span> км.<br>
+    <div class="wrapper-items block-2">
+        <div class="wrapper-item">
+            <div class="petrol-list">
+                Старый пробег: <input class="input-petrol-list" placeholder="<?=$balance['mileage']?>"> км. <br>
+                Новый пробег <span data-value="BT45" class="newMileage"></span>км. <br>
+                Остаток: <input class="input-petrol-list" placeholder="<?=$balance['balance']?>"> л. <br>
+                Дата последней заправки <span class="petrol lastOilDate"><?=$balance['last_date']?></span><br>
+                Всего бензина <span class="sumBalance" data-value="BT38"><?=$balance['balance']?></span> л.<br>
+                Осталось километров <span class="sumKm"><?=round($balance['balance']/(11/100))?></span> км.<br>
+            </div>
         </div>
-    </div>
-    
 
-    <div class="wrapper-item">
-        <div class="selectRoutes">
-        <span class="title">Первый путевой лист</span>
-        <br><br>
-        <input type="number" class="input-selectRoutes" value=""> Бензина залил <br><br>
-        <input type="date" class="input-selectRoutes"> Дата заправки<br>
-        <br>
-        <input type="date" class="input-selectRoutes"> Дата путевого листа<br><br>
-        <div class="checkbox-list">
-            <select>
-            <option data-id="0">не выбрано</option>
-            <?php foreach ($routes AS $route):?>
-            <option data-id="<?=$route[0]?>" data-km="<?=$route[3]?>"><?=$route[1]?> - <?=$route[2]?> (<?=$route[3]?>км)</option>
-            <?php endforeach;?>
-            </select> <input type="checkbox"> в обратную сторону
+
+        <div class="wrapper-item">
+            <div class="selectRoutes">
+                <div><span class="title">Первый путевой лист</span></div><br>
+                <input class="input-selectRoutes input-oil" placeholder="123"> Номер путевого листа <br><br>
+                <input type="number" class="input-selectRoutes input-oil" data-value="BT34"> Бензина залил <br><br>
+                <input type="date" class="input-selectRoutes input-oil-date"> Дата заправки<br>
+                <br>
+                <input type="date" class="input-selectRoutes new-date" data-value="date"> Дата путевого листа<br><br>
+                <div class="checkbox-list">
+                    <select>
+                        <option data-id="0">не выбрано</option>
+                        <?php foreach ($routes AS $route):?>
+                            <option data-id="<?=$route[0]?>" data-km="<?=$route[3]?>"><?=$route[1]?> - <?=$route[2]?> (<?=$route[3]?>км)</option>
+                        <?php endforeach;?>
+                    </select> <input type="checkbox"> в обратную сторону
+                </div>
+                <div class="checkbox-list">
+                    <select>
+                        <option data-id="0">не выбрано</option>
+                        <?php foreach ($routes AS $route):?>
+                            <option data-id="<?=$route[0]?>" data-km="<?=$route[3]?>"><?=$route[1]?> - <?=$route[2]?> (<?=$route[3]?>км)</option>
+                        <?php endforeach;?>
+                    </select> <input type="checkbox"> в обратную сторону
+                </div>
+                <div class="checkbox-list">
+                    <select>
+                        <option data-id="0">не выбрано</option>
+                        <?php foreach ($routes AS $route):?>
+                            <option data-id="<?=$route[0]?>" data-km="<?=$route[3]?>"><?=$route[1]?> - <?=$route[2]?> (<?=$route[3]?>км)</option>
+                        <?php endforeach;?>
+                    </select> <input type="checkbox"> в обратную сторону
+                </div>
+                <div class="checkbox-list">
+                    <select>
+                        <option data-id="0">не выбрано</option>
+                        <?php foreach ($routes AS $route):?>
+                            <option data-id="<?=$route[0]?>" data-km="<?=$route[3]?>"><?=$route[1]?> - <?=$route[2]?> (<?=$route[3]?>км)</option>
+                        <?php endforeach;?>
+                    </select> <input type="checkbox"> в обратную сторону
+                </div>
+                <div class="checkbox-list">
+                    <select>
+                        <option data-id="0">не выбрано</option>
+                        <?php foreach ($routes AS $route):?>
+                            <option data-id="<?=$route[0]?>" data-km="<?=$route[3]?>"><?=$route[1]?> - <?=$route[2]?> (<?=$route[3]?>км)</option>
+                        <?php endforeach;?>
+                    </select> <input type="checkbox"> в обратную сторону
+                </div>
+                <button class="btnGetDoc1">получить документ</button>
+            </div>
         </div>
-        <div class="checkbox-list">
-        <select>
-            <option data-id="0">не выбрано</option>
-            <?php foreach ($routes AS $route):?>
-                <option data-id="<?=$route[0]?>" data-km="<?=$route[3]?>"><?=$route[1]?> - <?=$route[2]?> (<?=$route[3]?>км)</option>
-            <?php endforeach;?>
-        </select> <input type="checkbox"> в обратную сторону
-        </div>
-        <div class="checkbox-list">
-            <select>
-                <option data-id="0">не выбрано</option>
-                <?php foreach ($routes AS $route):?>
-                    <option data-id="<?=$route[0]?>" data-km="<?=$route[3]?>"><?=$route[1]?> - <?=$route[2]?> (<?=$route[3]?>км)</option>
-                <?php endforeach;?>
-            </select> <input type="checkbox"> в обратную сторону
-        </div>
-        <div class="checkbox-list">
-            <select>
-                <option data-id="0">не выбрано</option>
-                <?php foreach ($routes AS $route):?>
-                    <option data-id="<?=$route[0]?>" data-km="<?=$route[3]?>"><?=$route[1]?> - <?=$route[2]?> (<?=$route[3]?>км)</option>
-                <?php endforeach;?>
-            </select> <input type="checkbox"> в обратную сторону
-        </div>
-        <div class="checkbox-list">
-            <select>
-                <option data-id="0">не выбрано</option>
-                <?php foreach ($routes AS $route):?>
-                    <option data-id="<?=$route[0]?>" data-km="<?=$route[3]?>"><?=$route[1]?> - <?=$route[2]?> (<?=$route[3]?>км)</option>
-                <?php endforeach;?>
-            </select> <input type="checkbox"> в обратную сторону
-        </div>
-     </div>   
+
     </div>
 
-</div>
 </div>
 
 
