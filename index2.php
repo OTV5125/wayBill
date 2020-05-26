9901f5597cb05c2e8664d2a5aac7433aae65642a
crysismzf <stas-petrushenko@mail.ru>
2020-05-26 23:01:26 +0300

    Добавлен еще один родительский эллемент

--- index2.php
+++ index2.php
@@ -43,7 +43,7 @@ $balance = $mysql->getBalance();
 
 <script type="text/babel">
     // верхний блок (с результатами)
-    class PetrolList1 extends React.Component {
+    class PetrolList extends React.Component {
         constructor (props) {
             super(props);
             this.state = {
@@ -119,7 +119,7 @@ $balance = $mysql->getBalance();
         }
     }
     // Нижний блок с полями ввода
-    class SelectRoutes1 extends React.Component {
+    class SelectRoutes extends React.Component {
         constructor(props) {
             super(props);
             this.addSelect = this.addSelect.bind(this);
@@ -234,7 +234,7 @@ $balance = $mysql->getBalance();
                             <div className="checkbox-list">
                                 { // здесь будет отрисовано необходимое кол-во компонентов
                                     this.state.selects.map((item) => (
-                                        <SelectBlock1 name="select-block"
+                                        <SelectBlock name="select-block"
                                                       key={item.id}
                                                       id={item.id}
                                                       handlerSelect={this.handlerSelect}
@@ -252,7 +252,7 @@ $balance = $mysql->getBalance();
         }
     }
     // Блок селектов
-    function SelectBlock1(props) {
+    function SelectBlock(props) {
             return (
             <div className="checkbox-list-items">
                 <select onChange={props.handlerSelect} id={props.id}>
@@ -268,7 +268,7 @@ $balance = $mysql->getBalance();
 
     }
 
-    class App  extends React.Component{
+    class PetrolBlock  extends React.Component{
         state = {
             addKM: '',
             numberList: '',
@@ -327,17 +327,25 @@ $balance = $mysql->getBalance();
         render() {
             const {addPetrol, petrolDate,addKM} = this.state;
             return (
+                <div className="wrapper-items">
+                    <PetrolList
+                        addPetrol = {addPetrol}
+                        petrolDate = {petrolDate}
+                        addKM = {addKM}
+                    />
+                    <SelectRoutes updateData={this.updateData}/>
+                </div>
+            )
+        }
+    }
+
+    class App extends React.Component {
+        render() {
+            return (
                 <React.Fragment>
                     <h1>Сервис посчитаем бензин</h1>
                     <div className="wrapper">
-                        <div className="wrapper-items block-1">
-                            <PetrolList1
-                                addPetrol = {addPetrol}
-                                petrolDate = {petrolDate}
-                                addKM = {addKM}
-                            />
-                            <SelectRoutes1 updateData={this.updateData}/>
-                        </div>
+                        <PetrolBlock/>
                     </div>
                 </React.Fragment>
             )
@@ -351,69 +359,7 @@ $balance = $mysql->getBalance();
 
 
 </script>
-<!--<script src="http://code.jquery.com/jquery-3.5.0.js"></script>-->
-<!--<script src="js/ajax.js"></script>-->
-<!--<script src="js/Math.js"></script>-->
-<!--<script src="js/routing.js"></script>-->
-<!--<script src="js/block.js"></script>-->
 
-<!--<script>-->
-<!--    window.onload = function () {-->
-<!--        new Routing('.block-1', --><?//=json_encode($balance)?><!--, --><?//=json_encode($routes)?><!--);-->
-<!--    }-->
-<!--</script>-->
-<?//=var_dump($balance)?>
-
-
-
-
-
-
-
-
-<!--    <div class="wrapper-items block-2">-->
-<!--        <div class="wrapper-item">-->
-<!--            <div class="petrol-list">-->
-<!--                Старый пробег: <input disabled class="input-petrol-list old-mileage" placeholder="--><?//= $balance['mileage'] ?><!--">-->
-<!--                км. <br>-->
-<!--                Новый пробег <span data-value="BT45" class="new-mileage"></span>км. <br>-->
-<!--                Остаток в начале дня: <input disabled class="input-petrol-list start-day-petrol"-->
-<!--                                             placeholder="--><?//= $balance['balance'] ?><!--"> л. <br>-->
-<!--                Остаток в конце дня: <span class="finish-day-petrol"> </span> л. <br>-->
-<!--                Дата последней заправки <span class="last-date-petrol">--><?//= $balance['last_date'] ?><!--</span><br>-->
-<!--                Всего бензина <span class="sum-petrol" data-value="BT38">--><?//= $balance['balance'] ?><!--</span> л.<br>-->
-<!--                Осталось километров <span class="rest-km">--><?//= round($balance['balance'] / (11 / 100)) ?><!--</span> км.<br>-->
-<!--            </div>-->
-<!--        </div>-->
-<!---->
-<!---->
-<!--        <div class="wrapper-item">-->
-<!--            <div class="select-routes">-->
-<!--                <div><span class="title">Первый путевой лист</span></div>-->
-<!--                <br>-->
-<!--                <input type="number" disabled class="input-select-routes number-list" value="1"> Номер путевого листа <br><br>-->
-<!--                <input type="number" class="input-select-routes input-petrol"> Бензина залил <br><br>-->
-<!--                <input type="date" class="input-select-routes input-petrol-date"> Дата заправки<br>-->
-<!--                <br>-->
-<!--                <input type="date" class="input-select-routes new-date-list"> Дата путевого листа<br><br>-->
-<!--                <div class="checkbox-list">-->
-<!--                    <div class="checkbox-list-items">-->
-<!--                        <select>-->
-<!--                            <option data-id="0">не выбрано</option>-->
-<!--                            --><?php //foreach ($routes AS $route): ?>
-<!--                                <option data-id="--><?//= $route[0] ?><!--" data-km="--><?//= $route[3] ?><!--">--><?//= $route[1] ?>
-<!--                                    - --><?//= $route[2] ?><!-- (--><?//= $route[3] ?><!--км)-->
-<!--                                </option>-->
-<!--                            --><?php //endforeach; ?>
-<!--                        </select> <input type="checkbox"> в обратную сторону-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--                <button class="add-route">Добавить маршрут</button>-->
-<!--                <button class="get-doc">получить документ</button>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</div>-->
 </body>
 </html>
 
